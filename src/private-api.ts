d6ba781a58adc9fbb47b0a72bb2233257ee57bba
Big Mogician <sunaoran@youzan.com>
2018-11-04 11:12:51 +0800

    publish 1.2.2

--- src/private-api.ts
+++ src/private-api.ts
@@ -2,7 +2,6 @@ export * from "./contracts/controller";
 export * from "./contracts/core";
 export * from "./contracts/di";
 export * from "./contracts/injectable";
-// export * from "./contracts/options";
 export * from "./contracts/pipe";
 export * from "./contracts/reflect";
 export * from "./contracts/source";
